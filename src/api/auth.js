68c820bf89d803cfb676385d0f6a3c960cfec20e
MilyB33 <milosz.bolewski.33@gmail.com>
2021-11-06 20:05:41 +0100

    chore: css improvements, added disable eslint rule to avoid warnings

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,6 +4,7 @@ export default axios.create({
   baseURL: 'http://localhost:3001',
   headers: {
     post: {
+      // eslint-disable-next-line
       ['Content-Type']: 'application/json',
     },
   },
